0993ce528be0a64a8a6c1a88f15e039c2a0550ab
Inge Becht <inge.becht91@gmail.com>
2013-10-27 22:49:02 +0100

    Added Orthello and Character Control basics

---
+++ Assets/Scripts/CharacterControls.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControls : MonoBehaviour {
+	
+	bool right = false;
+	bool left = false;
+	bool up = false;
+	bool down = false;
+	
+	float speed = 3;
+	float move_block = 150;
+	
+	GameObject camera; 	
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		Debug.Log (camera.transform.position);
+		handleInput();
+		handleMotion();
+	}
+	
+	void handleInput()
+	{
+		if(Input.GetKeyDown(KeyCode.D))
+		{	
+			right = true;
+		}
+		if(Input.GetKeyUp (KeyCode.D))
+		{
+			right = false;
+		}
+		
+		if(Input.GetKeyDown(KeyCode.A))
+		{	
+			left = true;
+		}
+		if(Input.GetKeyUp (KeyCode.A))
+		{
+			left = false;
+		}
+	}
+	
+	void handleMotion()
+	{
+		if(right)
+		{ 
+			transform.Translate(speed, 0, 0);
+			moveCamera(speed);
+		}
+		if(left)
+		{
+			transform.Translate(-speed, 0, 0);
+			moveCamera(-speed);
+			
+		}
+	}
+	
+	void moveCamera(float offset)
+	{
+		if(transform.position.x > camera.transform.position.x + move_block && right)
+		{
+			camera.transform.Translate (offset, 0, 0);
+		}	
+		if(transform.position.x < camera.transform.position.x - move_block && left)
+		{
+			camera.transform.Translate (offset, 0, 0);
+		}	
+	}
+	
+	
+}
